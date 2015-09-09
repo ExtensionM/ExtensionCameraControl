6a45fd843a0e4fdade982975bfaa3eb8e96f338c
sh4869 <nobuk4869@gmail.com>
2015-09-09 21:05:11 +0900

    Create HandlerMessageCodeEnum and create Children Find Code

--- app/src/main/java/net/sh4869/extensionandroidapp/activity/MainActivity.java
+++ app/src/main/java/net/sh4869/extensionandroidapp/activity/MainActivity.java
@@ -23,6 +23,7 @@ import net.sh4869.extensionandroidapp.R;
 import net.sh4869.extensionandroidapp.message.HandlerMessageCodeEnum;
 import net.sh4869.extensionandroidapp.websokcetdata.ExAuthResultWebSocketMessage;
 import net.sh4869.extensionandroidapp.websokcetdata.ExAuthWebSocketMessage;
+import net.sh4869.extensionandroidapp.websokcetdata.ExChild.ExChild;
 import net.sh4869.extensionandroidapp.websokcetdata.ExChild.ExChildFinder;
 import net.sh4869.extensionandroidapp.websokcetdata.ExChild.ExChildren;
 import net.sh4869.extensionandroidapp.websokcetdata.ExChildListMessage;
@@ -37,47 +38,59 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.channels.NotYetConnectedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static String WSTAG = "webSocket";
-    private String FILENAME = "userdata.json";
-    private String LOGIN_COMPLETE_MESSAGE = "Login Complete";
+    // TAG FOR WebSocket
+    private final String WSTAG = "WebSocket";
+    private final String ACTIVITY_TAG = "MainActivity";
+
+    // name of data
+    private final String FILENAME = "userdata.json";
+
+    private  String cameraChildGUID;
 
     private WebSocketClient mClient;
     private Handler mHandler;
-
-    public enum MessageCode {
-        Login_Success,
-        Login_fail;
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mHandler = new Handler() {
             @Override
-            public void handleMessage(Message message){
-                switch(message.what){
-                    case 0: // LOGIN SUCCESS
-                        Toast.makeText(MainActivity.this,getResources().getString(R.string.login_success),Toast.LENGTH_SHORT).show();
+            public void handleMessage(Message message) {
+                switch ((HandlerMessageCodeEnum) message.obj) {
+                    case LOGIN_SUCCESS: // LOGIN SUCCESS
+                        Toast.makeText(MainActivity.this, getResources().getString(R.string.login_success), Toast.LENGTH_SHORT).show();
                         break;
-                    case 1: // LOGIN FAIL
+                    case LOGIN_FAILED: // LOGIN FAIL
+                        Toast.makeText(MainActivity.this,getResources().getString(R.string.login_fail),Toast.LENGTH_SHORT).show();
                         new AlertDialog.Builder(MainActivity.this)
                                 .setTitle("Error")
-                                .setMessage("message: " + (String)message.obj)
+                                .setMessage("message: " + (String) message.obj)
                                 .setPositiveButton("OK", errorDialogButton)
                                 .show();
                         break;
-                    case 2:
+                    case CHILD_FOUND:
+                        Log.d(ACTIVITY_TAG,getResources().getString(R.string.child_found));
+                        Toast.makeText(MainActivity.this,getResources().getString(R.string.child_found),Toast.LENGTH_SHORT).show();
                         break;
+                    case CHILD_FOUND_MULTIPLE:
+                        Log.d(ACTIVITY_TAG,getResources().getString(R.string.child_found_multiple));
+                        Toast.makeText(MainActivity.this,getResources().getString(R.string.child_found_multiple),Toast.LENGTH_SHORT).show();
+                        break;
+                    case CHILD_NOT_FOUND:
+                        Log.d(ACTIVITY_TAG,getResources().getString(R.string.child_not_found));
+                        Toast.makeText(MainActivity.this,getResources().getString(R.string.child_not_found),Toast.LENGTH_SHORT).show();
                 }
             }
         };
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         File file = this.getFileStreamPath(FILENAME);
-        if(file.exists()) {
+        if (file.exists()) {
             if ("sdk".equals(Build.PRODUCT)) {
                 // エミュレータの場合はIPv6を無効
                 java.lang.System.setProperty("java.net.preferIPv6Addresses", "false");
@@ -88,25 +101,25 @@ public class MainActivity extends AppCompatActivity {
                 URI uri = new URI("ws://ec2-52-68-77-61.ap-northeast-1.compute.amazonaws.com:3000");
                 mClient = new WebSocketClient(uri) {
                     @Override
-                    public void onOpen (ServerHandshake handshakedata){
+                    public void onOpen(ServerHandshake handshakedata) {
                         Log.d(WSTAG, "onOpen");
                         sendLoginRequest();
                     }
 
                     @Override
-                    public void onMessage (String message){
+                    public void onMessage(String message) {
                         Log.d(WSTAG, "onMessage");
                         Log.d(WSTAG, "Message : " + message);
                         messageParser(message);
                     }
 
                     @Override
-                    public void onClose ( int code, String reason,boolean remote){
+                    public void onClose(int code, String reason, boolean remote) {
                         Log.d(WSTAG, "onClose");
                     }
 
                     @Override
-                    public void onError (Exception ex){
+                    public void onError(Exception ex) {
                         Log.d(WSTAG, "onError");
                         ex.printStackTrace();
                     }
@@ -114,16 +127,16 @@ public class MainActivity extends AppCompatActivity {
 
                 mClient.connect();
 
-            } catch (URISyntaxException e){
+            } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
         } else {
-            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         }
     }
 
-    private void messageParser(String message){
+    private void messageParser(String message) {
         Gson gson = new Gson();
         ExWebSocketMessage wsMessage = gson.fromJson(message, ExWebSocketMessage.class);
         try {
@@ -139,28 +152,28 @@ public class MainActivity extends AppCompatActivity {
                 default:
                     break;
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /// Onclick Lisnter for Dialog
-    DialogInterface.OnClickListener errorDialogButton = new DialogInterface.OnClickListener(){
+    DialogInterface.OnClickListener errorDialogButton = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
             callLoginAcitivty();
         }
     };
 
-    private void callLoginAcitivty(){
-        Intent intent = new Intent(getApplication(),LoginActivity.class);
+    private void callLoginAcitivty() {
+        Intent intent = new Intent(getApplication(), LoginActivity.class);
         startActivity(intent);
     }
 
     // ----------------------- * Web Auth * --------------------------------//
 
     /// Send Login Request Method
-    private void sendLoginRequest(){
+    private void sendLoginRequest() {
         InputStream fileInputStream;
         boolean sendsuccess = true;
         try {
@@ -177,52 +190,51 @@ public class MainActivity extends AppCompatActivity {
                 JsonObject userDataObject = userDataElement.getAsJsonObject();
 
                 String username = userDataObject.get("username").toString().replace("\"", "");
-                String password = userDataObject.get("password").toString().replace("\"","");
-                ExAuthWebSocketMessage authWSMessage = new ExAuthWebSocketMessage(username,password);
+                String password = userDataObject.get("password").toString().replace("\"", "");
+                ExAuthWebSocketMessage authWSMessage = new ExAuthWebSocketMessage(username, password);
                 try {
                     mClient.send(authWSMessage.toString());
-                } catch(NotYetConnectedException e){
+                } catch (NotYetConnectedException e) {
                     e.printStackTrace();
                 }
-            } catch (JsonParseException e){
+            } catch (JsonParseException e) {
                 e.printStackTrace();
                 sendsuccess = false;
-            } catch (IllegalStateException e){
+            } catch (IllegalStateException e) {
                 e.printStackTrace();
                 sendsuccess = false;
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             sendsuccess = false;
         }
-        if(!sendsuccess){
+        if (!sendsuccess) {
             callLoginAcitivty();
         }
     }
 
 
-
     /// Check Result of Login
-    private void checkAuthResult(ExAuthResultWebSocketMessage authResultMessage){
+    private void checkAuthResult(ExAuthResultWebSocketMessage authResultMessage) {
         try {
             if (authResultMessage.authResult()) {
                 Log.d(WSTAG, "Result Success");
                 sendChildListRequest();
-                Message resultMessage = createMessage(LOGIN_COMPLETE_MESSAGE, HandlerMessageCodeEnum.LOGIN_SUCCESS.ordinal());
+                Message resultMessage = createMessage(HandlerMessageCodeEnum.LOGIN_SUCCESS, HandlerMessageCodeEnum.LOGIN_SUCCESS.codeNumber());
                 mHandler.sendMessage(resultMessage);
             } else {
                 Log.d(WSTAG, "Auth Result Fail");
-                Message resultMessage = createMessage(authResultMessage.getErrorMessage(),HandlerMessageCodeEnum.LOGIN_FAILED.ordinal());
+                Message resultMessage = createMessage(HandlerMessageCodeEnum.LOGIN_FAILED, HandlerMessageCodeEnum.LOGIN_FAILED.codeNumber());
                 mHandler.sendMessage(resultMessage);
             }
-        } catch(IllegalStateException e){
+        } catch (IllegalStateException e) {
             e.printStackTrace();
         }
     }
 
-    Message createMessage(String messageObj,int whatCode){
+    Message createMessage(HandlerMessageCodeEnum messageCode, int whatCode) {
         Message message = Message.obtain();
-        message.obj = messageObj;
+        message.obj = messageCode;
         message.what = whatCode;
         return message;
     }
@@ -231,22 +243,34 @@ public class MainActivity extends AppCompatActivity {
     // ------------------------ * Child List * -------------------------------//
 
     /// Send Child Rist Request
-    private void sendChildListRequest(){
-        ExWebSocketMessage wsMessage = new ExWebSocketMessage("list",null);
-        try{
+    private void sendChildListRequest() {
+        ExWebSocketMessage wsMessage = new ExWebSocketMessage("list", null);
+        try {
             mClient.send(wsMessage.toString());
-        } catch (IllegalStateException e){
+        } catch (IllegalStateException e) {
             e.printStackTrace();
         }
     }
 
 
     //// TODO: 2015/09/07
-    private void checkChildListResult(ExChildListMessage message){
-        ExChildren children = ExChildFinder.searchChildren(message,"Camera");
-        if(children.commands.size() == 1){
-
+    private void checkChildListResult(ExChildListMessage message) {
+        ExChildren children = ExChildFinder.searchChildren(message, "Camera");
+        List<String> guidList = new ArrayList<String>();
+        Log.d(ACTIVITY_TAG,"Children Number : " + children.commands.size());
+        for (Map.Entry<String, ExChild> child : children) {
+            Log.d(ACTIVITY_TAG,"child guid : " + child.getKey());
+            guidList.add(child.getKey());
         }
+        Message messageData;
+        if (guidList.size() == 1) {
+            messageData = createMessage(HandlerMessageCodeEnum.CHILD_FOUND, HandlerMessageCodeEnum.CHILD_FOUND.codeNumber());
+        } else if (guidList.size() == 0) {
+            messageData = createMessage(HandlerMessageCodeEnum.CHILD_NOT_FOUND, HandlerMessageCodeEnum.CHILD_NOT_FOUND.codeNumber());
+        } else {
+            messageData = createMessage(HandlerMessageCodeEnum.CHILD_FOUND_MULTIPLE, HandlerMessageCodeEnum.CHILD_FOUND_MULTIPLE.codeNumber());
+        }
+        mHandler.sendMessage(messageData);
     }
 
 
@@ -271,7 +295,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 
 }
--- app/src/main/java/net/sh4869/extensionandroidapp/message/HandlerMessageCodeEnum.java
+++ app/src/main/java/net/sh4869/extensionandroidapp/message/HandlerMessageCodeEnum.java
@@ -6,9 +6,24 @@ import net.sh4869.extensionandroidapp.R;
  * Created by Nobuhiro on 2015/09/09.
  */
 public enum HandlerMessageCodeEnum {
-    LOGIN_SUCCESS,
-    LOGIN_FAILED,
-    CHILD_FOUND,
-    CHILD_FOUND_MULTIPLE,
-    CHILD_NOT_FOUND;
+    LOGIN_SUCCESS(1,"Login Complete!"),
+    LOGIN_FAILED(2,"Login Failed"),
+    CHILD_FOUND(3,"Target Child is found"),
+    CHILD_FOUND_MULTIPLE(4,"Target Children are found"),
+    CHILD_NOT_FOUND(5,"Target Children not fount");
+
+    private String message;
+    final private int codeNumber;
+
+    private HandlerMessageCodeEnum(int num,String message){
+        this.codeNumber = num;
+        this.message = message;
+    }
+
+    final public int codeNumber(){
+        return this.codeNumber;
+    }
+    public String message(){
+        return this.message;
+    }
 }
